3ee25bde5bc4b01d563a1f99c4fe7ea042658891
vikabibika-08 <zhurinaviktoria@yandex.ru>
2021-06-12 15:05:38 +0300

    hh

--- simpleapp/urls.py
+++ simpleapp/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import ProductsList  # импортируем наше представление
+from .views import ProductsList, ProductDetail
 
 urlpatterns = [
-    # path — означает путь. В данном случае путь ко всем товарам у нас останется пустым, позже станет ясно, почему
     path('', ProductsList.as_view()),
-    # т. к. сам по себе это класс, то нам надо представить этот класс в виде view. Для этого вызываем метод as_view
+    path('<int:pk>', ProductDetail.as_view()),
+    # pk — это первичный ключ товара, который будет выводиться у нас в шаблон
 ]
